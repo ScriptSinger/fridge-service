d19f071bab3b58a2d63dd0835457020eec0a1106
ScriptSinger <ufamasters102@gmail.com>
2026-02-05 18:10:02 +0500

    chore: use app name and url in jsonld

--- config/contacts.php
+++ config/contacts.php
@@ -3,17 +3,16 @@
 return [
 
     'phone_display' => env('CONTACT_PHONE_DISPLAY', '+7 (919) 609-34-85'),
-
     'phone_tel' => env('CONTACT_PHONE_TEL', '+79196093485'),
-    // Address
+
     'address_full' => env(
         'CONTACT_ADDRESS_FULL',
-        'ул. Мушникова, 11, Уфа, Респ. Башкортостан, Россия, 450043'
+        'ул. Мушникова, 11'
     ),
+
     'address_city' => env('CONTACT_ADDRESS_CITY', 'Уфа'),
     'address_country' => env('CONTACT_ADDRESS_COUNTRY', 'RU'),
     'area_served' => env('CONTACT_AREA_SERVED', 'Уфа'),
-    // Hours
+
     'opening_hours_display' => env('CONTACT_OPENING_HOURS_DISPLAY', 'Пн–Вс 10:00–20:00'),
-    'opening_hours_schema' => env('CONTACT_OPENING_HOURS_SCHEMA', 'Mo-Su 10:00-20:00'),
 ];
--- resources/views/components/seo/jsonld.blade.php
+++ resources/views/components/seo/jsonld.blade.php
@@ -1,35 +1,68 @@
-@php
-    $jsonld = [
-        '@context' => 'https://schema.org',
-        '@type' => 'LocalBusiness',
-        'name' => 'Рембыттехника',
-        'image' => 'https://site.ru/path/to/hero.png',
-        'telephone' => config('contacts.phone_display'),
-        'address' => [
-            '@type' => 'PostalAddress',
-            'addressLocality' => config('contacts.address_city'),
-            'addressCountry' => config('contacts.address_country'),
-        ],
-        'areaServed' => config('contacts.area_served'),
-        'url' => 'https://site.ru/',
-        'openingHours' => config('contacts.opening_hours_schema'),
-        'priceRange' => 'от 500 ₽',
-        'availableService' => [
-            [
-                '@type' => 'Service',
-                'name' => 'Ремонт холодильников',
-            ],
-            [
-                '@type' => 'Service',
-                'name' => 'Ремонт стиральных машин',
-            ],
-            [
-                '@type' => 'Service',
-                'name' => 'Ремонт другой бытовой техники',
-            ],
-        ],
-    ];
-@endphp
-<script type="application/ld+json">
+ @php
+     $jsonld = [
+         '@context' => 'https://schema.org',
+         '@type' => 'LocalBusiness',
+         'name' => config('app.name'),
+         'image' => rtrim(config('app.url'), '/') . '/assets/images/hero.png',
+         'url' => config('app.url'),
+
+         // ВАЖНО: только tel-формат
+         'telephone' => config('contacts.phone_tel'),
+
+         'priceRange' => '500-5000 RUB',
+
+         'address' => [
+             '@type' => 'PostalAddress',
+             'streetAddress' => config('contacts.address_full'),
+             'addressLocality' => config('contacts.address_city'),
+             'addressCountry' => config('contacts.address_country'),
+         ],
+
+         // ВАЖНО: не строка, а объект
+         'areaServed' => [
+             '@type' => 'City',
+             'name' => config('contacts.area_served'),
+         ],
+
+         // ВАЖНО: новый формат вместо openingHours
+         'openingHoursSpecification' => [
+             '@type' => 'OpeningHoursSpecification',
+             'dayOfWeek' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+             'opens' => '10:00',
+             'closes' => '20:00',
+         ],
+
+         // ВАЖНО: вместо availableService
+         'hasOfferCatalog' => [
+             '@type' => 'OfferCatalog',
+             'name' => 'Услуги ремонта',
+             'itemListElement' => [
+                 [
+                     '@type' => 'Offer',
+                     'itemOffered' => [
+                         '@type' => 'Service',
+                         'name' => 'Ремонт холодильников',
+                     ],
+                 ],
+                 [
+                     '@type' => 'Offer',
+                     'itemOffered' => [
+                         '@type' => 'Service',
+                         'name' => 'Ремонт стиральных машин',
+                     ],
+                 ],
+                 [
+                     '@type' => 'Offer',
+                     'itemOffered' => [
+                         '@type' => 'Service',
+                         'name' => 'Ремонт другой бытовой техники',
+                     ],
+                 ],
+             ],
+         ],
+     ];
+ @endphp
+
+ <script type="application/ld+json">
 {!! json_encode($jsonld, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!}
 </script>
