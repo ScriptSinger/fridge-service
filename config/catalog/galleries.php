b4efc5f0a164e096e354f1f235345893fed3a2bc
ScriptSinger <ufamasters102@gmail.com>
2026-02-17 20:17:42 +0500

    fix(config): remove leading whitespace in galleries config to restore session cookies and MoonShine login

--- config/catalog/galleries.php
+++ config/catalog/galleries.php
@@ -1,63 +1,63 @@
- <?php
+<?php
 
-    return [
+return [
 
-        'items' => [
+    'items' => [
 
-            [
-                'title'       => 'Ремонт после сильного шума',
-                'description' => 'Заменили подшипники и сальник, устранили вибрацию и посторонний шум.',
-                'device'      => null,
-                'brand'       => null,
-                'service'     => null,
-                'page'        => null,
-            ],
-
-            [
-                'title'       => 'Восстановление охлаждения',
-                'description' => 'Провели диагностику, заменили датчик температуры и настроили режимы.',
-                'device'      => null,
-                'brand'       => null,
-                'service'     => null,
-                'page'        => null,
-            ],
-
-            [
-                'title'       => 'Ремонт после протечки',
-                'description' => 'Заменили патрубок и герметизировали соединения, проверили цикл стирки.',
-                'device'      => null,
-                'brand'       => null,
-                'service'     => null,
-                'page'        => null,
-            ],
-
-            [
-                'title'       => 'Запуск после простоя',
-                'description' => 'Проверили пусковое реле и компрессор, восстановили стабильную работу.',
-                'device'      => null,
-                'brand'       => null,
-                'service'     => null,
-                'page'        => null,
-            ],
-
-            [
-                'title'       => 'Устранение ошибки управления',
-                'description' => 'Выполнили ремонт платы управления и проверку всех режимов.',
-                'device'      => null,
-                'brand'       => null,
-                'service'     => null,
-                'page'        => null,
-            ],
-
-            [
-                'title'       => 'Профилактика No Frost',
-                'description' => 'Очистили систему оттайки и заменили неисправные элементы.',
-                'device'      => null,
-                'brand'       => null,
-                'service'     => null,
-                'page'        => null,
-            ],
-
+        [
+            'title'       => 'Ремонт после сильного шума',
+            'description' => 'Заменили подшипники и сальник, устранили вибрацию и посторонний шум.',
+            'device'      => null,
+            'brand'       => null,
+            'service'     => null,
+            'page'        => null,
         ],
 
-    ];
+        [
+            'title'       => 'Восстановление охлаждения',
+            'description' => 'Провели диагностику, заменили датчик температуры и настроили режимы.',
+            'device'      => null,
+            'brand'       => null,
+            'service'     => null,
+            'page'        => null,
+        ],
+
+        [
+            'title'       => 'Ремонт после протечки',
+            'description' => 'Заменили патрубок и герметизировали соединения, проверили цикл стирки.',
+            'device'      => null,
+            'brand'       => null,
+            'service'     => null,
+            'page'        => null,
+        ],
+
+        [
+            'title'       => 'Запуск после простоя',
+            'description' => 'Проверили пусковое реле и компрессор, восстановили стабильную работу.',
+            'device'      => null,
+            'brand'       => null,
+            'service'     => null,
+            'page'        => null,
+        ],
+
+        [
+            'title'       => 'Устранение ошибки управления',
+            'description' => 'Выполнили ремонт платы управления и проверку всех режимов.',
+            'device'      => null,
+            'brand'       => null,
+            'service'     => null,
+            'page'        => null,
+        ],
+
+        [
+            'title'       => 'Профилактика No Frost',
+            'description' => 'Очистили систему оттайки и заменили неисправные элементы.',
+            'device'      => null,
+            'brand'       => null,
+            'service'     => null,
+            'page'        => null,
+        ],
+
+    ],
+
+];
