1315551aeac0c4a40c6f722a6ee1ba25130ddcbc
ScriptSinger <ufamasters102@gmail.com>
2026-02-26 12:51:31 +0500

    chore(seeders): move page and page type data to config/catalog and make seeders idempotent

---
+++ config/catalog/page_types.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    ['key' => 'home', 'name' => 'Главная', 'template' => 'pages.home', 'is_system' => true],
+    ['key' => 'about', 'name' => 'О компании', 'template' => 'pages.about', 'is_system' => true],
+    ['key' => 'contacts', 'name' => 'Контакты', 'template' => 'pages.contacts', 'is_system' => true],
+    ['key' => 'prices', 'name' => 'Цены', 'template' => 'pages.prices', 'is_system' => true],
+    ['key' => 'privacy-policy', 'name' => 'Политика конфиденциальности', 'template' => 'pages.legal', 'is_system' => true],
+    ['key' => 'personal-data-consent', 'name' => 'Согласие на обработку данных', 'template' => 'pages.legal', 'is_system' => true],
+];
--- database/seeders/PageTypeSeeder.php
+++ database/seeders/PageTypeSeeder.php
@@ -13,14 +13,7 @@ class PageTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        $pageTypes = [
-            ['key' => 'home', 'name' => 'Главная', 'template' => 'pages.home', 'is_system' => true],
-            ['key' => 'about', 'name' => 'О компании', 'template' => 'pages.about', 'is_system' => true],
-            ['key' => 'contacts', 'name' => 'Контакты', 'template' => 'pages.contacts', 'is_system' => true],
-            ['key' => 'prices', 'name' => 'Цены', 'template' => 'pages.prices', 'is_system' => true],
-            ['key' => 'privacy-policy', 'name' => 'Политика конфиденциальности', 'template' => 'pages.legal', 'is_system' => true],
-            ['key' => 'personal-data-consent', 'name' => 'Согласие на обработку данных', 'template' => 'pages.legal', 'is_system' => true],
-        ];
+        $pageTypes = config('catalog.page_types', []);
 
         foreach ($pageTypes as $type) {
             PageType::updateOrCreate(['key' => $type['key']], $type);
