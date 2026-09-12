78e4f4c237c39022a182db9f2de75613481706a4
ScriptSinger <ufamasters102@gmail.com>
2026-09-12 16:02:48 +0500

    fix: reorder gallery before prices on brand page to match device page

--- resources/views/pages/brand.blade.php
+++ resources/views/pages/brand.blade.php
@@ -2,10 +2,10 @@
     <x-ui.breadcrumbs :model="$brand" route="devices.brands.show" />
     <x-sections.hero :model="$brand" :h1="$brand->pivot->h1" :subtitle="$brand->pivot->subtitle" />
     <x-sections.brands.problems :problems="$problems" :device="$device" :brand="$brand" />
+    <x-sections.common.gallery :galleries="$galleries" />
     @if ($services->isNotEmpty())
         <x-sections.services.index :services="$services" :device="$device" :brand="$brand" />
     @endif
-    <x-sections.common.gallery :galleries="$galleries" />
     <x-sections.brands.error-codes :errorCodes="$errorCodes" :device="$device" :brand="$brand" />
     <x-sections.common.faq :faqs="$faqs" />
     <x-sections.contact :model="$brand" />
